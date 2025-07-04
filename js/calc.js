1586b44a71b7eab8c394e8f82b0bfad155af6d12
S-Asakoto <53332894+S-Asakoto@users.noreply.github.com>
2025-07-04 13:17:46 +0800

    Update calc.js

--- js/calc.js
+++ js/calc.js
@@ -1337,6 +1337,8 @@ function initMusic() {
                             if (result[c] == -Infinity)
                                 return "";
                         }
+                        else if (c == "currentPace")
+                            return result[c].toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits: 0}) + " pt";
                         return result[c] == undefined ? "" : b ? `
                         <tr>
                             <td colspan="2">(${_.replace(`{!${c}}`, `<span class="result" style="padding-left: 0; margin-left: 0;">${result[c]}</span>`)})</td>
